0e3d0432862d8610a2b34f167bca6dcf9018e34d
Kivol <kivoloid@gmail.com>
2015-08-31 18:50:37 +0900

    Initial version

--- README.md
+++ README.md
@@ -1,26 +1,26 @@
-# swint-builder-polymer
-Polymer web copmonent builder for Swint
+# swint-builder-html
+HTML builder for Swint
 
 **Warning: This is not the final draft yet, so do not use this until its official version is launched**
 
 ## Installation
 ```sh
-$ npm install --save swint-builder-polymer
+$ npm install --save swint-builder-html
 ```
 
 ## Options
 * `name` : `String`, default: `Project`
 * `inDir` : `String`, default: `path.dirname(require.main.filename)`
-* `imgMetaDir` : `String`, default: `path.join(path.dirname(require.main.filename), '../imgMeta')`
 * `outDir` : `String`, default: `path.join(path.dirname(require.main.filename), '../out')`
+* `minify` : `Boolean`, default: `true`
 * `variables` : `Object`, default: `{}`
+* `walkOption` : `Object`, default: `{ ext: 'html' }`
 
 ## Usage
 ```javascript
-buildPolymer({
+buildHTML({
 	name: 'Test',
 	inDir: path.join(__dirname, 'elements', 'target'),
-	imgMetaDir: path.join(__dirname, 'imgMeta'),
 	outDir: path.join(__dirname, 'out'),
 	variables: {}
 }, function() {
---
+++ test/index.js
@@ -0,0 +1,52 @@
+var os = require('os'),
+	fs = require('fs'),
+	path = require('path'),
+	assert = require('assert'),
+	swintHelper = require('swint-helper'),
+	buildHTML = require('../lib');
+
+describe('builder-html', function() {
+	it('Error when no callback', function() {
+		assert.throws(function() {
+			buildHTML({});
+		});
+	});
+
+	it('Error when inDir doesn\'t exist', function(done) {
+		buildHTML({
+			inDir: '/this-directory-does-not-exist'
+		}, function(err, res) {
+			assert.notEqual(err, null);
+			done();
+		});
+	});
+
+	it('Simple case', function(done) {
+		buildHTML({
+			name: 'Test',
+			inDir: path.join(__dirname, '../test_case/target'),
+			outDir: path.join(os.tmpdir(), 'swint-builder-html-out'),
+			variables: {
+				tmplVar: 'A'
+			}
+		}, function(err, res) {
+			assert.deepEqual(
+				fs.readFileSync(path.join(__dirname, '../test_result/screen1.html')),
+				fs.readFileSync(path.join(os.tmpdir(), 'swint-builder-html-out/screen1.html'))
+			);
+
+			assert.deepEqual(
+				fs.readFileSync(path.join(__dirname, '../test_result/screen2.html')),
+				fs.readFileSync(path.join(os.tmpdir(), 'swint-builder-html-out/screen2.html'))
+			);
+
+			done();
+		});
+	});
+
+	after(function() {
+		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-html-out/screen1.html'));
+		fs.unlinkSync(path.join(os.tmpdir(), 'swint-builder-html-out/screen2.html'));
+		fs.rmdirSync(path.join(os.tmpdir(), 'swint-builder-html-out'));
+	});
+});
